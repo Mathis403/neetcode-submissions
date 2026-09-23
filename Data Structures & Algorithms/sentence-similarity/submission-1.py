f9fd725ffe9e7ad27e3370a1307690a59e7175f0
Mathis FOURREAU <mathis.fourreau@edu.devinci.fr>
2026-09-23 18:37:45 +0200

    Add: sentence-similarity - submission-1

---
+++ Data Structures & Algorithms/sentence-similarity/submission-1.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+class Solution:
+    def areSentencesSimilar(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
+
+        d = defaultdict(list)
+
+        if len(sentence1) != len(sentence2):
+            return False
+
+        for element in similarPairs:
+
+            d[element[0]].append(element[1])
+            d[element[1]].append(element[0])
+
+        for i in range(len(sentence1)):
+            if sentence2[i] not in d[sentence1[i]] and sentence2[i] != sentence1[i]:
+                return False
+        
+        return True
+        
+
+
+
+
+        
